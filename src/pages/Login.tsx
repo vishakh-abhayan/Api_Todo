9034a3f82e4be9f2f8504eaa3ddd4d48bf05f9fa
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2023-07-17 04:30:05 +0530

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,38 @@
-import { Link } from "react-router-dom";
+import { useState, ChangeEvent, FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BsPersonFillCheck } from "react-icons/bs";
+import axios from "../axios";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/login/", {
+        username: formData.username,
+        password: formData.password,
+      });
+      const accessToken = response.data.access;
+      localStorage.setItem("accessToken", accessToken);
+      navigate(`/todo/${formData.username}`);
+    } catch (error) {
+      setErrorMessage("Invalid username or password");
+    }
+  };
+
   return (
     <div className="main_div">
       <div className="log_board">
@@ -9,15 +40,23 @@ function Login() {
           <BsPersonFillCheck size={50} />
           Login
         </h1>
-        <form className="input_field">
+        <form className="input_field" onSubmit={handleLogin}>
           <input
             placeholder="Username"
             type="text"
             name="username"
             autoComplete="off"
+            value={formData.username}
+            onChange={handleInputChange}
           />
-          <input placeholder="Password" type="password" name="password" />
-          {/* {errorMessage && <p className="error-message">{errorMessage}</p>} */}
+          <input
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
           <div className="log_buttons">
             <button type="submit" className="button_log">
               Login
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,17 +1,51 @@
 import "./Todo.css";
+import { useNavigate, useParams } from "react-router-dom";
 import { BsPersonCircle } from "react-icons/bs";
 import { IoTrashBin } from "react-icons/io5";
 import { FaPen } from "react-icons/fa";
+import { useEffect, useState, FormEvent } from "react";
+import axios from "../axios";
 
 function Todo() {
+  const { username } = useParams();
+  const navigate = useNavigate();
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const handleTodoSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "/todo/",
+        {
+          title: todoTitle,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      // Handle successful response here (e.g., show a success message)
+      console.log(response.data);
+    } catch (error) {
+      // Handle error here (e.g., show an error message)
+      console.error(error);
+    }
+  };
+
   return (
     <div className="app_todo">
       <div className="todo_cover">
         <h1 className="app_title ">ToDoNow</h1>
       </div>
       <div className="input_contain">
-        <form>
-          <input className="do_input" type="text" />
+        <form onSubmit={handleTodoSubmit}>
+          <input
+            className="do_input"
+            type="text"
+            value={todoTitle}
+            onChange={(e) => setTodoTitle(e.target.value)}
+          />
           <button type="submit" className="todo_check">
             <FaPen size={25} />
           </button>
@@ -20,14 +54,14 @@ function Todo() {
       <div className="todo_section">
         <div className="card_contain">
           <div className="todo_card">
-            <h1 className="todo_true"></h1>
+            <h1 className="todo_true">hklkj</h1>
             <IoTrashBin className="todo_bin" size={20} />
           </div>
         </div>
         <div className="todo_nav">
           <p className="nav_cop">
             <BsPersonCircle />
-            {/* {username} */}
+            {username}
           </p>
           <p className="nav_act">logout</p>
         </div>
